d830ea964ddda888b31a4b390ab66e763a145345
Philippe David <philippe.david@pj.ca>
2016-11-15 11:41:50 -0500

    Update support library to version 25 Rename main_menu.xml to image_picker_main_menu.xml fix the R.id.camera not found

--- imagepicker/src/main/java/com/esafirm/imagepicker/features/ImagePickerActivity.java
+++ imagepicker/src/main/java/com/esafirm/imagepicker/features/ImagePickerActivity.java
@@ -192,7 +192,7 @@ public class ImagePickerActivity extends AppCompatActivity
      */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.image_picker_menu_main, menu);
         return true;
     }
 
