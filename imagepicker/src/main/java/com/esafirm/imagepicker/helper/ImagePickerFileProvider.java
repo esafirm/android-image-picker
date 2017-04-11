6c0abb3c0559694e1bb509d2ec0a25a53394d418
Douglas Iacovelli <douglas.iacovelli@gmail.com>
2017-04-11 14:18:59 -0300

    removes unnecessary comment

--- imagepicker/src/main/java/com/esafirm/imagepicker/helper/ImagePickerFileProvider.java
+++ imagepicker/src/main/java/com/esafirm/imagepicker/helper/ImagePickerFileProvider.java
@@ -2,9 +2,5 @@ package com.esafirm.imagepicker.helper;
 
 import android.support.v4.content.FileProvider;
 
-/**
- * Created by douglasiacovelli on 11/04/17.
- */
-
 public class ImagePickerFileProvider extends FileProvider {
 }
