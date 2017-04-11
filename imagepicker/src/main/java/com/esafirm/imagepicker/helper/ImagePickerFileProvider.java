6ffd518d0dea43324c3802e887ddaab62a78e6d7
Douglas Iacovelli <douglas.iacovelli@gmail.com>
2017-04-11 11:20:53 -0300

    renames classes to avoid name conflicts with other projects

---
+++ imagepicker/src/main/java/com/esafirm/imagepicker/helper/ImagePickerFileProvider.java
@@ -0,0 +1,10 @@
+package com.esafirm.imagepicker.helper;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by douglasiacovelli on 11/04/17.
+ */
+
+public class ImagePickerFileProvider extends FileProvider {
+}
