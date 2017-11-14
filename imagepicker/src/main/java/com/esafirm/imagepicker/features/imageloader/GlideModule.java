836f52c1e80a7f9fc601520f81c2f4e48f8781b3
Esa Firman <esafirm21@gmail.com>
2017-11-14 08:06:14 +0700

    Instead of extending AppGlideModule, ImagePicker now extending LibraryGlideModule

--- imagepicker/src/main/java/com/esafirm/imagepicker/features/imageloader/GlideModule.java
+++
@@ -1,7 +0,0 @@
-package com.esafirm.imagepicker.features.imageloader;
-
-import com.bumptech.glide.module.AppGlideModule;
-
-@com.bumptech.glide.annotation.GlideModule
-public class GlideModule extends AppGlideModule {
-}
---
+++ imagepicker/src/main/java/com/esafirm/imagepicker/features/imageloader/ImagePickerGlideModule.java
@@ -0,0 +1,7 @@
+package com.esafirm.imagepicker.features.imageloader;
+
+import com.bumptech.glide.module.LibraryGlideModule;
+
+@com.bumptech.glide.annotation.GlideModule
+public class ImagePickerGlideModule extends LibraryGlideModule {
+}
