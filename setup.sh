5754308e3034e9f05de7e98676472d388e453b80
lexter <lex.duxcast@gmail.com>
2019-02-21 19:52:34 +0300

    Added android sdk license

--- setup.sh
+++ setup.sh
@@ -4,3 +4,12 @@ echo "include ':imagepicker'" > settings.gradle
 echo "include ':rximagepicker'" >> settings.gradle
 
 cat settings.gradle
+
+cd $ANDROID_HOME
+mkdir -p licenses
+
+cat << EOF >> licenses/android-sdk-license
+8933bad161af4178b1185d1a37fbf41ea5269c55
+d56f5187479451eabf01fb78af6dfcb131a6481e
+24333f8a63b6825ea9c5514f83c2829b004d1fee
+EOF
