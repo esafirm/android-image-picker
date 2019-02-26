40ee769c1fcce99afc64d1f6fda6717d89e0cf6d
lexter <lex.duxcast@gmail.com>
2019-02-26 12:09:09 +0300

    Changed accepting SDK license for jitpack to more correct

--- setup.sh
+++ setup.sh
@@ -5,11 +5,4 @@ echo "include ':rximagepicker'" >> settings.gradle
 
 cat settings.gradle
 
-cd $ANDROID_HOME
-mkdir -p licenses
-
-cat << EOF >> licenses/android-sdk-license
-8933bad161af4178b1185d1a37fbf41ea5269c55
-d56f5187479451eabf01fb78af6dfcb131a6481e
-24333f8a63b6825ea9c5514f83c2829b004d1fee
-EOF
+yes | $ANDROID_HOME/tools/bin/sdkmanager "build-tools;28.0.3"
