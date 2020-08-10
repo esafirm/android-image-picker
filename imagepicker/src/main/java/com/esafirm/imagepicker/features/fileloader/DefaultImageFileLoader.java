211a4dd0e6dbaf723fc7f077f1e7f5a7fff29dfb
Esa Firman <esafirm21@gmail.com>
2020-08-10 23:53:12 +0700

    remove unused import

--- imagepicker/src/main/java/com/esafirm/imagepicker/features/fileloader/DefaultImageFileLoader.java
+++ imagepicker/src/main/java/com/esafirm/imagepicker/features/fileloader/DefaultImageFileLoader.java
@@ -1,6 +1,5 @@
 package com.esafirm.imagepicker.features.fileloader;
 
-import android.content.ContentUris;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
