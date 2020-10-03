82eb0b8e1b094534710def73cbb42166daa86b35
Esa Firman <esafirm21@gmail.com>
2020-10-03 15:13:54 +0700

    build ready

--- sample/src/main/java/com/esafirm/sample/SampleApplication.java
+++ sample/src/main/java/com/esafirm/sample/SampleApplication.java
@@ -2,16 +2,10 @@ package com.esafirm.sample;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
-
 public class SampleApplication extends Application {
-  @Override public void onCreate() {
-    super.onCreate();
-    if (LeakCanary.isInAnalyzerProcess(this)) {
-      // This process is dedicated to LeakCanary for heap analysis.
-      // You should not init your app in this process.
-      return;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //NO-OP
     }
-    LeakCanary.install(this);
-  }
 }
