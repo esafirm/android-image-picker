f611465b611db84af5e1c392f212149484631e08
alifgiant <alif.akbar@bukalapak.com>
2020-10-12 11:45:57 +0800

    folder to kt

---
+++ imagepicker/src/main/java/com/esafirm/imagepicker/model/Folder.kt
@@ -0,0 +1,10 @@
+package com.esafirm.imagepicker.model
+
+import java.util.ArrayList
+
+/**
+ * Created by boss1088 on 8/22/16.
+ */
+class Folder(private var bucket: String) {
+    private var images: ArrayList<Image> = ArrayList()
+}
