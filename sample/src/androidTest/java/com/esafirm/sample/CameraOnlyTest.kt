d9aa1ae53d9efc518cd757f69291a767bf24cdec
Esa Firman <esafirm21@gmail.com>
2020-10-19 16:44:22 +0700

    Fix camera on q (#306)
    * handle uri on Q
    
    * add test for camera only

---
+++ sample/src/androidTest/java/com/esafirm/sample/CameraOnlyTest.kt
@@ -0,0 +1,56 @@
+package com.esafirm.sample
+
+
+import android.widget.ScrollView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.rule.GrantPermissionRule
+import androidx.test.runner.AndroidJUnit4
+import com.esafirm.sample.matchers.hasDrawable
+import com.schibsted.spain.barista.intents.BaristaIntents.mockAndroidCamera
+import com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class CameraOnlyTest {
+
+    @Rule
+    @JvmField
+    var testRule = ActivityTestRule(MainActivity::class.java)
+
+    @Rule
+    @JvmField
+    var grantPermissionRule = GrantPermissionRule.grant(
+        "android.permission.CAMERA",
+        "android.permission.WRITE_EXTERNAL_STORAGE"
+    )
+
+    @Test
+    fun cameraOnlyTestTwo() {
+        Intents.init()
+        mockAndroidCamera()
+        clickOn(R.id.button_camera)
+        Intents.release()
+
+        clickOn(R.id.text_view)
+
+        val imageView = onView(
+            allOf(ViewMatchers.withParent(allOf(withId(R.id.container),
+                ViewMatchers.withParent(IsInstanceOf.instanceOf(ScrollView::class.java)))),
+                isDisplayed()))
+
+        imageView.check(ViewAssertions.matches(isDisplayed()))
+        imageView.check(ViewAssertions.matches(hasDrawable()))
+    }
+}
