afb9e94f6239a6e78d211b4eeca85527ff95266b
Michael Samuel <michaelsam00@yahoo.com>
2021-06-01 17:09:54 +0200

    -fix fragment not attached to context exception

--- sample/src/main/java/com/esafirm/sample/MainFragment.kt
+++ sample/src/main/java/com/esafirm/sample/MainFragment.kt
@@ -1,24 +1,27 @@
 package com.esafirm.sample
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
-import com.esafirm.imagepicker.features.ImagePickerConfig
-import com.esafirm.imagepicker.features.ImagePickerMode
-import com.esafirm.imagepicker.features.ReturnMode
-import com.esafirm.imagepicker.features.registerImagePicker
+import com.esafirm.imagepicker.features.*
 import com.esafirm.sample.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
     private lateinit var binding: FragmentMainBinding
 
-    private val imagePickerLauncher = registerImagePicker {
-        val firstImage = it.firstOrNull() ?: return@registerImagePicker
-        Glide.with(binding.imgFragment)
-            .load(firstImage.uri)
-            .into(binding.imgFragment)
+    private lateinit var imagePickerLauncher: ImagePickerLauncher
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        imagePickerLauncher = registerImagePicker(context) {
+            val firstImage = it.firstOrNull() ?: return@registerImagePicker
+            Glide.with(binding.imgFragment)
+                .load(firstImage.uri)
+                .into(binding.imgFragment)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
