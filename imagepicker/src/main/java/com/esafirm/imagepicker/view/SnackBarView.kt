8067709d20d5527c1154efad60b215a85d732c6e
Esa Firman <esafirm21@gmail.com>
2021-06-19 14:29:17 +0700

    fix broken snackbar

--- imagepicker/src/main/java/com/esafirm/imagepicker/view/SnackBarView.kt
+++ imagepicker/src/main/java/com/esafirm/imagepicker/view/SnackBarView.kt
@@ -2,11 +2,9 @@ package com.esafirm.imagepicker.view
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
 import android.view.animation.Interpolator
 import android.widget.RelativeLayout
 import androidx.annotation.StringRes
-import androidx.core.view.ViewCompat
 import androidx.interpolator.view.animation.FastOutLinearInInterpolator
 import com.esafirm.imagepicker.R
 import kotlinx.android.synthetic.main.ef_imagepikcer_snackbar.view.*
@@ -22,9 +20,6 @@ class SnackBarView @JvmOverloads constructor(
 
     init {
         inflate(context, R.layout.ef_imagepikcer_snackbar, this)
-        val padding = context.resources.getDimensionPixelSize(R.dimen.ef_spacing_double)
-        setPadding(padding, 0, padding, 0)
-
         if (!isInEditMode) {
             val height = context.resources.getDimensionPixelSize(R.dimen.ef_height_snackbar)
             translationY = height.toFloat()
@@ -32,38 +27,14 @@ class SnackBarView @JvmOverloads constructor(
         }
     }
 
-    fun setText(@StringRes textResId: Int) {
-        txtCaption?.setText(textResId)
-    }
-
-    fun setOnActionClickListener(@StringRes textId: Int, onClickListener: OnClickListener) {
-        val mTextId = if (textId == 0) R.string.ef_ok else textId
-        btnAction?.let {
-            setText(mTextId)
-            setOnClickListener { v: View? -> hide { onClickListener.onClick(v) } }
-        }
-    }
-
     fun show(@StringRes textResId: Int, onClickListener: OnClickListener) {
-        setText(textResId)
-        setOnActionClickListener(0, onClickListener)
-        ViewCompat.animate(this)
-            .translationY(0f)
+        txtCaption.text = context.getString(textResId)
+        btnAction.setOnClickListener(onClickListener)
+
+        animate().translationY(0f)
             .setDuration(ANIM_DURATION.toLong())
             .setInterpolator(INTERPOLATOR)
             .alpha(1f)
-    }
-
-    fun hide() {
-        hide(null)
-    }
-
-    private fun hide(runnable: Runnable?) {
-        ViewCompat.animate(this)
-            .translationY(height.toFloat())
-            .setDuration(ANIM_DURATION.toLong())
-            .alpha(0.5f)
-            .withEndAction(runnable)
     }
 
     companion object {
