3979d5d6646e079f438ce8be3fe4cde2d515a603
Doruk Kangal <dorukkangal@gmail.com>
2021-09-24 17:33:43 +0300

    Update dependency versions

--- setup.sh
+++ setup.sh
@@ -4,5 +4,3 @@ echo "include ':imagepicker'" > settings.gradle
 echo "include ':rximagepicker'" >> settings.gradle
 
 cat settings.gradle
-
-yes | $ANDROID_HOME/tools/bin/sdkmanager "build-tools;28.0.3"
