da17e7ba2af29f83c78f88eaefc3ad011e4b5f05
Esa Firman <esafirm21@gmail.com>
2022-10-23 22:43:34 +0700

    clean up

--- imagepicker/src/main/java/com/esafirm/imagepicker/features/ImagePickerFragment.kt
+++ imagepicker/src/main/java/com/esafirm/imagepicker/features/ImagePickerFragment.kt
@@ -10,7 +10,6 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Parcelable
 import android.provider.Settings
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -32,7 +31,6 @@ import com.esafirm.imagepicker.helper.IpLogger
 import com.esafirm.imagepicker.helper.state.fetch
 import com.esafirm.imagepicker.model.Folder
 import com.esafirm.imagepicker.model.Image
-import java.util.ArrayList
 
 class ImagePickerFragment : Fragment() {
 
@@ -81,9 +79,9 @@ class ImagePickerFragment : Fragment() {
         if (::interactionListener.isInitialized.not()) {
             throw RuntimeException(
                 "ImagePickerFragment needs an " +
-                "ImagePickerInteractionListener. This will be set automatically if the " +
-                "activity implements ImagePickerInteractionListener, and can be set manually " +
-                "with fragment.setInteractionListener(listener)."
+                    "ImagePickerInteractionListener. This will be set automatically if the " +
+                    "activity implements ImagePickerInteractionListener, and can be set manually " +
+                    "with fragment.setInteractionListener(listener)."
             )
         }
 
@@ -273,6 +271,7 @@ class ImagePickerFragment : Fragment() {
             shouldShowRequestPermissionRationale(permission) -> {
                 requestPermissionLauncher.launch(permission)
             }
+
             else -> {
                 if (!preferences.isPermissionRequested()) {
                     preferences.setPermissionIsRequested()
@@ -384,7 +383,6 @@ class ImagePickerFragment : Fragment() {
         private const val STATE_KEY_SELECTED_IMAGES = "Key.SelectedImages"
 
         private const val RC_CAPTURE = 2000
-        private const val RC_PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE = 23
 
         fun newInstance(config: ImagePickerConfig): ImagePickerFragment {
             val args = Bundle().apply {
