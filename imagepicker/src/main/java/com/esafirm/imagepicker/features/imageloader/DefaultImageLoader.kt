fc7dde0344e0b038f3dfadd851eb5da94fc0b47c
Hung <>
2022-10-24 11:45:28 +0700

    fix(grid): GC Issue

--- imagepicker/src/main/java/com/esafirm/imagepicker/features/imageloader/DefaultImageLoader.kt
+++ imagepicker/src/main/java/com/esafirm/imagepicker/features/imageloader/DefaultImageLoader.kt
@@ -5,12 +5,14 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
 import com.esafirm.imagepicker.R
+import com.esafirm.imagepicker.helper.getImageSize
 import com.esafirm.imagepicker.model.Image
 
 class DefaultImageLoader : ImageLoader {
     override fun loadImage(image: Image, imageView: ImageView, imageType: ImageType) {
         Glide.with(imageView.context)
             .load(image.uri)
+            .override(imageView.context.getImageSize())
             .apply(RequestOptions
                 .placeholderOf(if (imageType == ImageType.FOLDER) R.drawable.ef_folder_placeholder else R.drawable.ef_image_placeholder)
                 .error(if (imageType == ImageType.FOLDER) R.drawable.ef_folder_placeholder else R.drawable.ef_image_placeholder)
--- imagepicker/src/main/java/com/esafirm/imagepicker/helper/ViewUtils.kt
+++ imagepicker/src/main/java/com/esafirm/imagepicker/helper/ViewUtils.kt
@@ -1,21 +1,42 @@
 package com.esafirm.imagepicker.helper
 
 import android.content.Context
+import android.content.res.Configuration
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.view.View
 import androidx.core.content.ContextCompat
 import com.esafirm.imagepicker.R
 
+
+const val GRID_3 = 3
+const val GRID_5 = 5
+
 object ViewUtils {
     fun getArrowIcon(context: Context): Drawable? {
-        val backResourceId: Int = if (Build.VERSION.SDK_INT >= 17 && context.resources.configuration.layoutDirection == View.LAYOUT_DIRECTION_RTL) {
-            // For right-to-left layouts, pick the drawable that points to the right (forward).
-            R.drawable.ef_ic_arrow_forward
-        } else {
-            // For left-to-right layouts, pick the drawable that points to the left (back).
-            R.drawable.ef_ic_arrow_back
-        }
+        val backResourceId: Int =
+            if (Build.VERSION.SDK_INT >= 17 && context.resources.configuration.layoutDirection == View.LAYOUT_DIRECTION_RTL) {
+                // For right-to-left layouts, pick the drawable that points to the right (forward).
+                R.drawable.ef_ic_arrow_forward
+            } else {
+                // For left-to-right layouts, pick the drawable that points to the left (back).
+                R.drawable.ef_ic_arrow_back
+            }
         return ContextCompat.getDrawable(context.applicationContext, backResourceId)
+    }
+}
+
+fun Context.getImageSize(): Int {
+    return this.resources.displayMetrics.widthPixels / getRecyclerViewColumn()
+}
+
+fun Context.getRecyclerViewColumn(): Int {
+    val orientation = this.resources.configuration.orientation
+    return if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        // In landscape
+        GRID_5
+    } else {
+        // In portrait
+        GRID_3
     }
 }
